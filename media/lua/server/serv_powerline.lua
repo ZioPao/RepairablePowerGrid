78adc0e70666b1ddef8b664cfa35b1ca97fdd263
Pao <picchetti1998@gmail.com>
2023-03-03 09:53:19 +0100

    first commit

---
+++ media/lua/server/serv_powerline.lua
@@ -0,0 +1,125 @@
+--[[⠀
+----------------------------------------------------------------------------------------------------------------------------⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                       ⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⣰⣦⣀⣀⠀⠠⠄⠄⠠⠀⠀⢀⣠⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⡿⢿⣦⠀⠀⠀⠀⠀⠀⠀⢀⡷⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⣩⡿⠋⠙⠉⢩⡿⠟⠀⠀⠀⠀⠀⠀⢀⣶⣾⠟⠋⠛⣿⣷⡄⠀⠀⠀⠀⠀⣴⣿⠂⠀⠀⣼⡿⠀⠀⠀⠀⠀⠀⢀⣠⡴⠶⣤⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠦⠤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡿⠋⠀⢸⠏⠀⠀⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⢠⡇⠀⠀⠀⢠⣾⠋⠀⠀⠀⠀⣸⠀⠀⠀⠀⠀⠀⠀⢠⣿⠋⠀⠀⠀⠀⠛⠛⠁⠀⠀⠀⢀⣾⣿⠏⠀⠀⢀⡿⠃⠀⠀⠀⠀⠀⠶⠋⠁⢀⣰⠟⠀⠀⠀⠀⠀⠀⠀⣹⣿⠀⠀⠀⠀⣨⣿⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣾⡟⢀⣠⣤⣴⣶⠆⠀⠀⠀⠀⠀⢰⠃⠀⠀⠀⠀⠀⣾⠀⠀⠀⣰⣿⠃⠀⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⠋⠀⠀⢀⡾⠁⠀⠀⠀⠀⠀⠀⠀⣠⣶⣯⣅⡀⠀⠀⠀⠀⠀⠀⠀⣿⡇⢀⣠⠴⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⡟⠀⠛⠉⣠⣾⠃⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⠀⢿⣤⡴⢺⢻⠃⠀⠀⠀⠀⠀⢸⡿⠁⠀⠀⠀⠀⠀⠀⢿⠀⠀⠀⠀⢀⣤⠀⠀⠀⠀⠀⢀⣿⣇⡤⠤⢤⣾⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢻⡆⠀⠀⠀⠀⠀⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠀⢀⣼⠇⠀⠀⠀⠀⠀⠀⣰⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢂⠇⠀⠀⠀⠀⠀⠀⣾⡅⠀⠀⠀⠀⠀⠀⠀⢸⣧⡀⠀⠀⣼⠇⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠐⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠃⠀⠀⠀⠀⠀⢰⡏⠘⣦⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⢧⣤⡶⠟⢻⠀⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⠏⠀⠀⠀⠀⠀⠀⢰⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⣶⡾⠋⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⢰⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠞⠁⠀⠀⠀⠀⠀⠀⠁⠀⠈⠙⠂⠔⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⠀⢀⣿⣤⠶⠒⠒⠁⠀⠀⠀⠀⢠⠏⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⠐⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+----------------------------------------------------------------------------------------------------------------------------
+Glytc3r⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+Head Admin
+Signs of Sickness PZ Server
+https://discord.gg/U5umjC6jDV
+
+Please contact me if you need to hire someone to do mods or other design related tasks
+https://www.paypal.me/glytch3r
+
+https://www.glytch3r.com
+discord id: Glytch3r#2892
+
+The fusebox icon I used is from the tile made by ThrottleKitty (with permission)
+----------------------------------------------------------------------------------------------------------------------------
+--]]
+
+local powerHours = SandboxVars.ReduceGen.CD
+print(powerHours)
+
+RepairLineRecipes = RepairLineRecipes or {}
+RepairLineRecipes.GetItemTypes = RepairLineRecipes.GetItemTypes or {}
+RepairLineRecipes.OnCanPerform = RepairLineRecipes.OnCanPerform or {}
+RepairLineRecipes.OnCreate = RepairLineRecipes.OnCreate or {}
+RepairLineRecipes.OnGiveXP = RepairLineRecipes.OnGiveXP or {}
+RepairLineRecipes.OnTest = RepairLineRecipes.OnTest or {}
+RepairLineRecipes.OnCreate.repairGen = function(_, _, player)
+
+  getSandboxOptions():getOptionByName("ElecShutModifier"):setValue(2147483647)
+  powerHours = SandboxVars.ReduceGen.CD + 48
+  powerHours = powerHours + 24
+  player:Say(getText("This will hold for another " .. powerHours .. "hours "))
+  print("PowerGrid: ON! " .. powerHours .. "hours ")
+end
+
+
+--------------------------
+
+local function TurnPowerOff()
+  -- turn power off using sandbox vars like you wrote above
+  getSandboxOptions():getOptionByName("ElecShutModifier"):setValue(-1)
+  print("Turning power off!")
+end
+
+local function IsPowerOn()
+  -- return whether the power is on, can check the sandbox vars
+  local esm = getSandboxOptions():getOptionByName("ElecShutModifier"):getValue()
+  if esm == -1 then
+    return false
+  else
+    return true
+  end
+end
+
+
+--------------------------------
+
+local function UpdatePowerCheck()
+  powerHours = powerHours - 1
+  -- print("PowerGrid: might break in " .. cdexpire .. "hours" )
+
+  if powerHours <= 0 then
+    if IsPowerOn() then
+      if SandboxVars.ReduceGen.Rate <= ZombRand(1, 100) then
+        TurnPowerOff()
+        --	print("PowerGrid: Broke!")
+      else
+        powerHours = 1
+        --    print("PowerGrid: CRITICAL!")
+        --			print("PowerGrid: might break in " .. cdexpire .. "hour" )
+      end
+    end
+  end
+end
+Events.EveryHours.Add(UpdatePowerCheck)
+
+-- function checkPower()
+--   if not IsPowerOn() then
+--     getPlayer():Say(getText("PowerGrid Needs Fixing"));
+--     return;
+--   end
+--   if IsPowerOn() and powerHours == 1 then
+--     getPlayer():Say(getText("PowerGrid: might break in " .. powerHours .. "hour"));
+--     return;
+--   end
+
+--   if IsPowerOn() and powerHours >= 0 then
+--     getPlayer():Say(getText("PowerGrid: might break in " .. powerHours .. "hours"));
+--     return;
+--   end
+-- end
+
+-- function craftFuseBox()
+--   if SandboxVars.ReduceGen.Craftable == false then
+--     print(getPlayer():getUsername() .. " not allowed to Crafted a FuseBox");
+--     getPlayer():Say(getText("Crafting this item is not allowed by this server"));
+--     getPlayer():getInventory():AddItem("Radio.ElectricWire");
+--     getPlayer():getInventory():AddItem("Radio.ElectricWire");
+--     getPlayer():getInventory():AddItem("Radio.ElectricWire");
+--     getPlayer():getInventory():AddItem("Radio.ElectricWire");
+--     getPlayer():getInventory():AddItem("Radio.ElectricWire");
+--     getPlayer():getInventory():AddItem("Base.SheetMetal");
+--     getPlayer():getInventory():AddItem("Base.SheetMetal");
+--     getPlayer():getInventory():AddItem("Base.SheetMetal");
+--     getPlayer():getInventory():AddItem("Base.SheetMetal");
+--     getPlayer():getInventory():AddItem("Base.SheetMetal");
+--     return
+--   end
+--   local fb = InventoryItemFactory.CreateItem("Fuse.FuseBox");
+--   fb:setActualWeight(10);
+--   fb:setWeight(10);
+--   getPlayer():getInventory():AddItem(fb);
+--   print(getPlayer():getUsername() .. " Crafted a FuseBox");
+-- end
